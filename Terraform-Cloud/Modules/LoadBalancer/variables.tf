cba5a45b5a486a69086b09408504bb4fdc6957e5
Katoria H <87483461+theCaptN21@users.noreply.github.com>
2023-02-20 15:18:31 -0500

    Update variables.tf

--- Terraform-Cloud/Modules/LoadBalancer/variables.tf
+++ Terraform-Cloud/Modules/LoadBalancer/variables.tf
@@ -1,10 +1,13 @@
-variable "tg_protocol" {
-  default = "HTTP"
-}
-
-variable "tg_port" {
-  default = 80
-}
+variable "tg_port" {}
+variable "tg_protocol" {}
+variable "lb_healthy_threshold" {}
+variable "lb_unhealthy_threshold" {}
+variable "lb_timeout" {}
+variable "lb_interval" {}
+variable "vpc_id" {}
+variable "database_asg" {}
+variable "public_subnet" {}
+variable "web_sg" {}
 
 variable "listener_protocol" {
   default = "HTTP"
@@ -13,8 +16,3 @@ variable "listener_protocol" {
 variable "listener_port" {
   default = 80
 }
-
-variable "vpc_id" {}
-variable "database_asg" {}
-variable "public_subnet" {}
-variable "web_sg" {}
